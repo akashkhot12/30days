374f03493831b1a1d76496357bf81309ff8ad870
akashkhot12 <akashkhot03@gmail.com>
2024-07-18 17:55:21 +0530

    adding new items

--- react/basic-react/src/List.jsx
+++ react/basic-react/src/List.jsx
@@ -2,13 +2,38 @@ import React, { useState } from "react";
 import DataList from "./Data.json";
 export default function List() {
   const [nData, setnData] = useState(DataList);
+
+  const handleRemove = (itemsId) => {
+    setnData(nData.filter(item => item.id!==itemsId))
+  };
+
+  const handleUpdate = (itemsId) => {
+    setnData(nData.map(item=>{
+      if(item.id === itemsId){
+        return{name:"Akash Khot"}
+      }
+      else{
+        return item;
+      }
+    }))
+  };
   return (
     <div>
-      <ul>
-        {nData.map((items) => {
-          <li key={items.id}>{items.id}</li>;
-        })}
-      </ul>
+      <ol>
+        {nData.map((items) => (
+          <li key={items.id}>
+            Name = {items.name} <br />
+            <span>Address = {items.address}</span>
+            <br />
+            <br />
+            <button onClick={()=>handleUpdate(items.id)}>Update</button>
+            <button onClick={()=>handleRemove(items.id)}>Delete</button>
+            <br />
+            <br />
+          </li>
+        ))}
+        <button onClick={() => setnData([])}>Clear</button>
+      </ol>
     </div>
   );
 }
